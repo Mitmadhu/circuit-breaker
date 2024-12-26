3441b3b6a3d39fdc227d3d5d7000919e2046249e
mitmadhu <madhuvermamit2k15@gmail.com>
2024-12-26 13:40:52 +0530

    custom impl of CB

--- main.go
+++ main.go
@@ -4,50 +4,32 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/eapache/go-resiliency/breaker"
+	circuitbreaker "github.com/mitmadhu/circuit-breaker/circuit_breaker"
 )
 
-type callWithCB struct {
-	cbObj *breaker.Breaker
-}
-
-func NewCB() callWithCB {
-	return callWithCB{
-		cbObj: breaker.New(3, 2, time.Second),
-	}
-}
-
-func (cb *callWithCB) withCB(testCall func() error) error {
-
-	err := cb.cbObj.Run(testCall)
-
-	if err == nil {
-		println("success in api call")
-		return nil
-	}
-
-	if err == breaker.ErrBreakerOpen {
-		println("circuit is open")
-	} else {
-		println("circuit is close and got error: " + err.Error())
-	}
-
-	return err
-
-}
+var (
+	cb = circuitbreaker.NewCB(time.Second, 3, 2)
+)
 
 func main() {
-	cb := NewCB()
 
-	for i := 0; i < 50; i++ {
-		cb.withCB(func() error {
+	for i := 0; i < 30; i++ {
+		err := cb.Run(func() error {
 			if i%4 == 0 {
-				return fmt.Errorf("[%d] error in api call", i)
+				return fmt.Errorf("[%d] error in call", i)
 			}
 			return nil
 		})
+		if err != nil {
+			fmt.Printf("[%d] error in call, error : %s \n", i, err.Error())
+		} else {
+			fmt.Printf("[%d] call was successful\n", i)
+		}
 
 		time.Sleep(100 * time.Millisecond)
 	}
 
+}
+
+func abcd() {
 }
